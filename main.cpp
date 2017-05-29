81c3bd61b95f41e0aa011c5811cc0ec22fac4ca5
mhi <mauricio_ishida@hotmail.com>
2017-05-29 17:59:35 -0300

    right

--- main.cpp
+++ main.cpp
@@ -4,9 +4,47 @@
 //-----./a.out -W nome_do_arquivo_saida-----
 #include <iostream>
 #include <fstream>
-
+#include <unistd.h>
 
 using namespace std;
+
+void order66(){
+    if(geteuid()!= 0){
+        cout<<"YOU DON'T HAVE THE POWERS YET!!"<<endl;
+    } else {
+        cout<<"EXECUTING ORDER 66!!";
+        system("  beep -l 350 -f 392 -D 100 --new -l 350 -f 392 -D 100 --new -l 350 -f 392 -D \\\n"
+                   "  100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 350 \\\n"
+                   "  -f 392 -D 100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new \\\n"
+                   "  -l 700 -f 392 -D 100 --new -l 350 -f 587.32 -D 100 --new -l 350 -f 587.32 -D \\\n"
+                   "  100 --new -l 350 -f 587.32 -D 100 --new -l 250 -f 622.26 -D 100 --new -l 25 \\\n"
+                   "  -f 466.2 -D 100 --new -l 350 -f 369.99 -D 100 --new -l 250 -f 311.1 -D 100 \\\n"
+                   "  --new -l 25 -f 466.2 -D 100 --new -l 700 -f 392 -D 100 --new -l 350 -f 784 \\\n"
+                   "  -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 392 -D 100 --new -l 350 -f \\\n"
+                   "  784 -D 100 --new -l 250 -f 739.98 -D 100 --new -l 25 -f 698.46 -D 100 --new \\\n"
+                   "  -l 25 -f 659.26 -D 100 --new -l 25 -f 622.26 -D 100 --new -l 50 -f 659.26 \\\n"
+                   "  -D 400 --new -l 25 -f 415.3 -D 200 --new -l 350 -f 554.36 -D 100 --new -l \\\n"
+                   "  250 -f 523.25 -D 100 --new -l 25 -f 493.88 -D 100 --new -l 25 -f 466.16 -D \\\n"
+                   "  100 --new -l 25 -f 440 -D 100 --new -l 50 -f 466.16 -D 400 --new -l 25 -f \\\n"
+                   "  311.13 -D 200 --new -l 350 -f 369.99 -D 100 --new -l 250 -f 311.13 -D 100 \\\n"
+                   "  --new -l 25 -f 392 -D 100 --new -l 350 -f 466.16 -D 100 --new -l 250 -f 392 \\\n"
+                   "  -D 100 --new -l 25 -f 466.16 -D 100 --new -l 700 -f 587.32 -D 100 --new -l \\\n"
+                   "  350 -f 784 -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 392 -D 100 --new \\\n"
+                   "  -l 350 -f 784 -D 100 --new -l 250 -f 739.98 -D 100 --new -l 25 -f 698.46 -D \\\n"
+                   "  100 --new -l 25 -f 659.26 -D 100 --new -l 25 -f 622.26 -D 100 --new -l 50 \\\n"
+                   "  -f 659.26 -D 400 --new -l 25 -f 415.3 -D 200 --new -l 350 -f 554.36 -D 100 \\\n"
+                   "  --new -l 250 -f 523.25 -D 100 --new -l 25 -f 493.88 -D 100 --new -l 25 -f \\\n"
+                   "  466.16 -D 100 --new -l 25 -f 440 -D 100 --new -l 50 -f 466.16 -D 400 --new \\\n"
+                   "  -l 25 -f 311.13 -D 200 --new -l 350 -f 392 -D 100 --new -l 250 -f 311.13 -D \\\n"
+                   "  100 --new -l 25 -f 466.16 -D 100 --new -l 300 -f 392.00 -D 150 --new -l 250 \\\n"
+                   "  -f 311.13 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 700 -f 392");
+    }
+
+}
+
+
+
+
 
 typedef struct abit{
     unsigned X:1;
@@ -307,6 +345,13 @@ int main(int argc, char *argv[]){
 
     }else if(op=="-W"){
         MakeBfile("out.haming",entrada);
+
+    }else if(op=="Order"){
+        if(entrada=="66"){
+            order66();
+        }
+
     }
+
     return 0;
 }
